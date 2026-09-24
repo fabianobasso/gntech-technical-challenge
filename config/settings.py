c58eb7d7a207162aafa6f2542ad584c2331c5a29
Fabiano Basso <fabiano.basso2017@gmail.com>
2026-09-24 18:15:17 -0300

    chore: dockerize Django application

--- config/settings.py
+++ config/settings.py
@@ -138,6 +138,10 @@ MAILERS = {
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
 }
 
 SPECTACULAR_SETTINGS = {
